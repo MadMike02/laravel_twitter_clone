34efd1bbdc6e26a3afb80e026aa7513329af2cec
sahiljassal <sahiljassal9198@gmail.com>
2024-02-25 00:35:37 +0530

    active menu links by Route::is() methods

--- ideas/resources/views/terms.blade.php
+++ ideas/resources/views/terms.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.layout')
 
+
 @section('content')
-    LOREM IPSUM
+<div class="row">
+    <div class="col-3">
+        @include('shared.left-sidebar')
+    </div>
+    <div class="col-6">
+        LOREM IPSUM
+    </div>
+    <div class="col-3">
+        @include('shared.search-bar')
+        @include('shared.follow-box')
+    </div>
+</div>
 @endsection
